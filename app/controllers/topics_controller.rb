4caa4a95030dee89e9dd53489f18a6405248c19a
Wen-Tien Chang <ihower@gmail.com>
2017-03-28 11:30:34 +0800

    Ajax like/unlike

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,21 +5,28 @@ class TopicsController < ApplicationController
   end
 
   def like
-    topic = Topic.find( params[:id] )
+    @topic = Topic.find( params[:id] )
 
-    unless topic.is_liked_by(current_user)
-      Like.create( :topic => topic, :user => current_user )
+    unless @topic.is_liked_by(current_user)
+      Like.create( :topic => @topic, :user => current_user )
     end
 
-    redirect_to topics_path
+    respond_to do |format|
+      format.js
+      format.html { redirect_to topics_path }
+    end
   end
 
   def unlike
-    topic = Topic.find(params[:id])
-    like = topic.find_like(current_user)
+    @topic = Topic.find(params[:id])
+    like = @topic.find_like(current_user)
 
     like.destroy
-    redirect_to topics_path
+
+    respond_to do |format|
+      format.js { render "like" } # like.js.erb
+      format.html { redirect_to topics_path }
+    end
   end
 
   def destroy
